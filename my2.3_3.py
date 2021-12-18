87848ec6e28ca851c5248513c089720031ee85d7
Nina <dedyuhina1997@list.ru>
2021-12-18 20:36:28 +0500

    lesson stepik 2.3.3

---
+++ my2.3_3.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    button = browser.find_element_by_xpath("//button[@type='submit']")
+    button.click()
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+    pole = browser.find_element_by_id('answer')
+    pole.send_keys(y)
+    button = browser.find_element_by_xpath("//button[@type='submit']")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(15)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
